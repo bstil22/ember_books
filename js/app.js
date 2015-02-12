da46e8402f1b3cd962f58515d78cd7a0dc0e8907
bstil22 <bradydynamite@hotmail.com>
2015-02-12 15:21:50 -0800

    working

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
-App = Ember.Application.create()
+window.App = Ember.Application.create();
 
-  App.Router.map(function() {
-    LOG_TRANSITIONS: true
-    this.route('aboutme')
-    this.route('projects')
-    this.route ('project', {path: "/projects/:project_id"})
-  });
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
-  App.ProjectsRoute = Ember.Route.extend({
-      model: function() {
-        return [
-        {id: 1, name: 'GovData', link: 'https://github.com/bstil22/gov_data_project target="_blank"'},
-        {id: 2, name: 'gReddit', link: 'https://github.com/bstil22/gReddit target="_blank"'},
-        {id: 3, name: 'StockChart', link: 'https://github.com/bstil22/js_stockchart target="_blank"'}        
-        ]
-      }
-  });
   
